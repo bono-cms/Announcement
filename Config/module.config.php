69fd55c66d2e1e4c3972ee6d529f075bf204797c
David Yang <daworld.ny@gmail.com>
2016-04-30 22:35:34 +0500

    Replaced raw path with route

--- Config/module.config.php
+++ Config/module.config.php
@@ -12,7 +12,7 @@
 return array(
     'module' => array(
         'name'  => 'Announcement',
-        'url'   => '/admin/module/announcement',
+        'route' => 'Announcement:Admin:Browser@indexAction',
         'icon'  => 'fa fa-comments fa-5x',
         'order' => 1,
         'description' => 'Announcement module allows you to show different marketing announces on your site'
