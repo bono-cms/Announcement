26445e9f19b28e3fc736916404d5da1e6fb2572b
David Yang <daworld.ny@gmail.com>
2016-05-05 23:51:43 +0500

    Replaced static admin segment with its placeholder

--- Config/routes.php
+++ Config/routes.php
@@ -10,64 +10,63 @@
  */
 
 return array(
-    
     '/module/announcement/(:var)' => array(
         'controller' => 'Announce@indexAction'
     ),
     
-    '/admin/module/announcement' => array(
+    '/%s/module/announcement' => array(
         'controller' => 'Admin:Browser@indexAction',
     ),
     
-    '/admin/module/announcement/category/add' => array(
+    '/%s/module/announcement/category/add' => array(
         'controller' => 'Admin:Category@addAction'
     ),
     
-    '/admin/module/announcement/category/edit/(:var)' => array(
+    '/%s/module/announcement/category/edit/(:var)' => array(
         'controller' => 'Admin:Category@editAction'
     ),
     
-    '/admin/module/announcement/category/save' => array(
+    '/%s/module/announcement/category/save' => array(
         'controller' => 'Admin:Category@saveAction',
         'disallow' => array('guest')
     ),
     
-    '/admin/module/announcement/category/delete/(:var)' => array(
+    '/%s/module/announcement/category/delete/(:var)' => array(
         'controller' => 'Admin:Category@deleteAction',
         'disallow' => array('guest')
     ),
     
-    '/admin/module/announcement/category/view/(:var)' => array(
+    '/%s/module/announcement/category/view/(:var)' => array(
         'controller' => 'Admin:Browser@categoryAction'
     ),
     
-    '/admin/module/announcement/category/view/(:var)/page/(:var)' => array(
+    '/%s/module/announcement/category/view/(:var)/page/(:var)' => array(
         'controller' => 'Admin:Browser@categoryAction'
     ),
     
-    '/admin/module/announcement/page/(:var)' => array(
+    '/%s/module/announcement/page/(:var)' => array(
         'controller' => 'Admin:Browser@indexAction'
     ),
     
-    '/admin/module/announcement/announce/tweak.ajax' => array(
+    '/%s/module/announcement/announce/tweak.ajax' => array(
         'controller' => 'Admin:Browser@tweakAction',
         'disallow' => array('guest')
     ),
     
-    '/admin/module/announcement/announce/delete/(:var)' => array(
+    '/%s/module/announcement/announce/delete/(:var)' => array(
         'controller' => 'Admin:Announce@deleteAction',
         'disallow' => array('guest')
     ),
     
-    '/admin/module/announcement/announce/add' => array(
+    '/%s/module/announcement/announce/add' => array(
         'controller' => 'Admin:Announce@addAction'
     ),
     
-    '/admin/module/announcement/announce/edit/(:var)' => array(
+    '/%s/module/announcement/announce/edit/(:var)' => array(
         'controller' => 'Admin:Announce@editAction'
     ),
     
-    '/admin/module/announcement/announce/save' => array(
+    '/%s/module/announcement/announce/save' => array(
         'controller'    => 'Admin:Announce@saveAction',
         'disallow' => array('guest')
     )
