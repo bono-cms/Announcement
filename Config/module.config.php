c3d588963be0a875af9426fe764477fbadf103c7
David Yang <daworld.ny@gmail.com>
2016-08-01 22:27:01 +0500

    Removed extras from module configuration file

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,20 +1,9 @@
 <?php
 
-/**
- * This file is part of the Bono CMS
- * 
- * Copyright (c) No Global State Lab
- * 
- * For the full copyright and license information, please view
- * the license file that was distributed with this source code.
- */
-
 return array(
-    'module' => array(
-        'name'  => 'Announcement',
-        'route' => 'Announcement:Admin:Browser@indexAction',
-        'icon'  => 'fa fa-comments fa-5x',
-        'order' => 1,
-        'description' => 'Announcement module allows you to show different marketing announces on your site'
-    )
+ 'name'  => 'Announcement',
+ 'route' => 'Announcement:Admin:Browser@indexAction',
+ 'icon'  => 'fa fa-comments fa-5x',
+ 'order' => 1,
+ 'description' => 'Announcement module allows you to show different marketing announces on your site'
 );
