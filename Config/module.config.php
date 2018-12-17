216ad70918d292902de8fc1f9a53b998fbfe13be
dave-yang <daworld.ny@gmail.com>
2018-12-17 18:33:22 +0500

    Minor fixes. Use shared actions partial

--- Config/module.config.php
+++ Config/module.config.php
@@ -4,7 +4,6 @@ return array(
  'caption'  => 'Announcement',
  'name'  => 'Announcement',
  'route' => 'Announcement:Admin:Browser@indexAction',
- 'icon'  => 'fa fa-comments fa-5x',
  'order' => 1,
  'description' => 'Announcement module allows you to show different marketing announces on your site'
 );
