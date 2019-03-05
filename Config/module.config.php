84af6134f6323e42ffea47d2687433e807e44e75
dave-yang <daworld.ny@gmail.com>
2019-03-05 23:13:46 +0500

    Updated module config

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,9 +1,28 @@
 <?php
 
+/**
+ * Module configuration container
+ */
+
 return array(
- 'caption'  => 'Announcement',
- 'name'  => 'Announcement',
- 'route' => 'Announcement:Admin:Browser@indexAction',
- 'order' => 1,
- 'description' => 'Announcement module allows you to show different marketing announces on your site'
+    'caption'  => 'Announcement',
+    'description' => 'Announcement module allows you to show different marketing announces on your site',
+    'menu' => array(
+        'name'  => 'Announcement',
+        'icon' => '',
+        'items' => array(
+            array(
+                'route' => 'Announcement:Admin:Browser@indexAction',
+                'name' => 'View all announces',
+            ),
+            array(
+                'route' => 'Announcement:Admin:Announce@addAction',
+                'name' => 'Add new announce'
+            ),
+            array(
+                'route' => 'Announcement:Admin:Category@addAction',
+                'name' => 'Add a category'
+            )
+        )
+    )
 );
